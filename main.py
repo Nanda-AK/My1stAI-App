7b70b3aab6d24eb0b9557958b550937b9601e563
Nanda-AK <59553626+Nanda-AK@users.noreply.github.com>
2025-03-15 16:34:36 +0530

    Update main.py
    With my Python code

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import streamlit as st
 import os
 
 os.environ['GOOGLE_API_KEY'] = st.secrets['GOOGLE_API_KEY']
-#os.environ['GOOGLE_API_KEY']="AIzaSyDKEj08oZ514Zj1yxX-6AW8QclnkqXRCEc"
 
+"""
 # Create prompt template for generating tweets
 
 tweet_template = "Give me {number} tweets on {topic}"
@@ -20,8 +20,38 @@ gemini_model = ChatGoogleGenerativeAI(model = "gemini-1.5-flash-latest")
 
 # Create LLM chain using the prompt template and model
 tweet_chain = tweet_prompt | gemini_model
+"""
 
+### My Python Code to generate Tweet 
+# Generate using Prompt Template
+# Import PromptTemplate for Template
 
+tweet_template ="""
+'Give me {number} tweets on Topic {topic}'
+give me 5 hashtag per tweet along with popularity counter e.g. #NatureLover(97456)
+
+Use below format as e.g
+Tweet sample 1:
+This is Great New, he looks awsome in this photo.
+
+Tweet sample 2:
+This is crazy, i can not belive this has happned to him.
+"""
+
+tweet_prompt = PromptTemplate(template = tweet_template, input_variables =['number','topic'])
+
+#Initializing the Google Gemini Model Flatsh 1.5
+gemini_model = ChatGoogleGenerativeAI(model = "gemini-1.5-flash-latest")
+
+#Create LLM Chain using theprompt template and Model
+tweet_chain = tweet_prompt | gemini_model
+
+#Example of using the LLM Chain
+#response = tweet_chain.invoke({"number": 5, "topic": "Nature is my God"})
+
+#print(f"\n\n\nBelow is the sampel tweet from LLM\n\n\n {response.content}")
+
+### Below code on streamlit ####
 import streamlit as st
 
 st.header("🐦 Tweet Generator")
